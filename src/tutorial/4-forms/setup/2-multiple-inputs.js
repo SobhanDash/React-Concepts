d783805c1e40c066c7ebfc924b7e5a81346eda92
Sobhan Dash <52308005+SobhanDash@users.noreply.github.com>
2021-05-18 20:56:18 +0530

    feat: handling multiple inputs

--- src/tutorial/4-forms/setup/2-multiple-inputs.js
+++ src/tutorial/4-forms/setup/2-multiple-inputs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react"
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -6,63 +6,85 @@ import React, { useState } from 'react';
 // value, onChange
 // dynamic object keys
 
-const ControlledInputs = () => {
-  const [firstName, setFirstName] = useState('');
-  const [email, setEmail] = useState('');
-  const [people, setPeople] = useState([]);
+const ControlledInputs = () => {		
+	// const [firstName, setFirstName] = useState('');
+	// const [email, setEmail] = useState('');
+	// const [age, setAge] = useState('');
+	const [person, setPerson] = useState({ firstName: "", email: "", age: "" })
+	const [people, setPeople] = useState([])
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (firstName && email) {
-      const person = { id: new Date().getTime().toString(), firstName, email };
-      console.log(person);
-      setPeople((people) => {
-        return [...people, person];
-      });
-      setFirstName('');
-      setEmail('');
-    } else {
-      console.log('empty values');
+  //takes multiple inputs and handles them
+	const handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    // console.log(name, value) 
+    //we spread the object so if we are adding name we don't delete email or age from the list
+    setPerson({ ...person, [name]: value })
+  }
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+    if(person.firstName && person.email && person.age){
+      const newPerson = {...person, id: new Date().getTime().toString()}
+      setPeople([...people, newPerson]);
+      setPerson({ firstName:"", email:"", age:"" });
     }
-  };
-  return (
-    <>
-      <article>
-        <form className='form' onSubmit={handleSubmit}>
-          <div className='form-control'>
-            <label htmlFor='firstName'>Name : </label>
-            <input
-              type='text'
-              id='firstName'
-              name='firstName'
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-          </div>
-          <div className='form-control'>
-            <label htmlFor='email'>Email : </label>
-            <input
-              type='email'
-              id='email'
-              name='email'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <button type='submit'>add person</button>
-        </form>
-        {people.map((person, index) => {
-          const { id, firstName, email } = person;
-          return (
-            <div className='item' key={id}>
-              <h4>{firstName}</h4>
-              <p>{email}</p>
-            </div>
-          );
-        })}
-      </article>
-    </>
-  );
-};
+	}
 
-export default ControlledInputs;
+	return (
+		<>
+			<article>
+				<form className='form' onSubmit={handleSubmit}>
+					<div className='form-control'>
+						<label htmlFor='firstName'>Name : </label>
+						<input
+							type='text'
+							id='firstName'
+							name='firstName'
+							value={person.firstName}
+							onChange={handleChange}
+						/>
+					</div>
+
+					<div className='form-control'>
+						<label htmlFor='email'>Email : </label>
+						<input
+							type='email'
+							id='email'
+							name='email'
+							value={person.email}
+							onChange={handleChange}
+						/>
+					</div>
+
+					<div className='form-control'>
+						<label htmlFor='age'>Age : </label>
+						<input
+							type='text'
+							id='age'
+							name='age'
+							value={person.age}
+							onChange={handleChange}
+						/>
+					</div>
+
+					<button type='submit' onClick={handleSubmit}>
+						add person
+					</button>
+				</form>
+				{people.map((person, index) => {
+					const { id, firstName, email, age } = person
+					return (
+						<div className='item' key={id}>
+							<h4>{firstName}</h4>
+							<h4>{age}</h4>
+							<p>{email}</p>
+						</div>
+					)
+				})}
+			</article>
+		</>
+	)
+}
+
+export default ControlledInputs
