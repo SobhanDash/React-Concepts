f1b7ca4e474bfca10d5d9e84dfa40b67162b5bdd
Sobhan Dash <52308005+SobhanDash@users.noreply.github.com>
2021-06-07 18:22:33 +0530

    Update useRef file

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,37 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react"
 
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
-};
+  //useRef 
+  const refContainer = useRef(null)
+  const divContainer = useRef(null)
+  
 
-export default UseRefBasics;
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(refContainer.current.value)
+    console.log(divContainer.current)
+  }
+  //console.log(refContainer)
+  //since useRef does'nt trigger re-renders. useEffect focuses on the input right from the first render
+  useEffect(() => {
+    console.log(refContainer.current)
+    refContainer.current.focus()
+  })
+	return (
+		<>
+			<form className='form' onSubmit={handleSubmit}>
+				<div>
+					<input type='text' ref={refContainer} />
+					<button type='submit'>Submit</button>
+				</div>
+			</form>
+      <div ref={divContainer}>Hello World</div>
+		</>
+	)
+}
+
+export default UseRefBasics
