69917e881afd25d6d4c7c25dde1aaf01de22d568
SobhanDash <sobhan7dash@gmail.com>
2021-07-06 13:03:03 +0530

    updated customHook setup

--- src/tutorial/9-custom-hooks/setup/1-fetch-example.js
+++ src/tutorial/9-custom-hooks/setup/1-fetch-example.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { useFetch } from './2-useFetch'
 
 // ATTENTION!!!!!!!!!!
@@ -6,23 +6,20 @@ import { useFetch } from './2-useFetch'
 const url = 'https://course-api.com/javascript-store-products'
 
 const Example = () => {
-  const [loading, setLoading] = useState(true)
-  const [products, setProducts] = useState([])
-
-  const getProducts = async () => {
-    const response = await fetch(url)
-    const products = await response.json()
-    setProducts(products)
-    setLoading(false)
-  }
-
-  useEffect(() => {
-    getProducts()
-  }, [url])
+  const { loading, products } = useFetch(url)
   console.log(products)
   return (
     <div>
       <h2>{loading ? 'loading...' : 'data'}</h2>
+      <ul>
+        {products.map((product) => {
+          return (
+            <li key={product.id} className="item">
+              {product.fields.name}
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
