2bf976e7e596e1058449fb547904b586bb533df1
SobhanDash <sobhan7dash@gmail.com>
2021-07-06 13:03:19 +0530

    updated prop-types setup

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React from 'react'
+import PropTypes from 'prop-types'
+import defaultImage from '../../../assets/default-image.jpeg'
 
-const Product = () => {
-  return <article className='product'>single product</article>;
-};
+const Product = ({ image, name, price }) => {
+  //image exits along with url
+  const url = image && image.url
+  return (
+    <article className="product">
+      {/* if image available then use else deafult, same with name */}
+      <img src={url || defaultImage} alt={name || 'default name'} />
+      <h4>{name}</h4>
+      <p>${price || 14.67}</p>
+    </article>
+  )
+}
 
-export default Product;
+//type of props
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+}
+
+// defaultProps can also be used in place of short circuit exal
+// Product.defaultProps = {
+//   name: 'default name',
+//   price: 3.99,
+//   image: defaultImage,
+// };
+
+export default Product
